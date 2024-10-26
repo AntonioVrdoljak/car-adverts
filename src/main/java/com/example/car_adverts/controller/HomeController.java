2845225761631ae0b3421eed001d53fe06bdb47b
Antonio <tonivrdoljak165@gmail.com>
2024-10-26 23:27:08 +0200

    Implemented HomeController with root paht endpoint

---
+++ src/main/java/com/example/car_adverts/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.car_adverts.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Car Advert API!";
+    }
+}

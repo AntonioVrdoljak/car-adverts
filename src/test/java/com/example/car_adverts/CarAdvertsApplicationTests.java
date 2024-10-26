74b6bf2dadd2683817d02d65313ecbf330bca19b
Antonio <tonivrdoljak165@gmail.com>
2024-10-26 16:41:40 +0200

    Initial commit: setup spring boot project

---
+++ src/test/java/com/example/car_adverts/CarAdvertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.car_adverts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarAdvertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

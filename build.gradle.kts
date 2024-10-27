c90c33beea833b630d36be90839f3beafe098c7e
Antonio <tonivrdoljak165@gmail.com>
2024-10-27 20:33:22 +0100

    Added Swagger-UI documentation

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
